c64910076c412c91d8d6a7460c525f80813015d9
Lucas Tavares <tavaresluc@gmail.com>
2022-09-20 22:00:06 -0300

    created routes and fetch api

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,7 @@ import { Text } from 'react-native';
 import  Background from './src/components/Background';
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black} from '@expo-google-fonts/inter';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 export default function App() {
@@ -18,7 +18,7 @@ export default function App() {
       <StatusBar
       barStyle="light-content" backgroundColor="transparent" translucent/>
 
-    {fontsLoaded ? <Home /> : <Loading />}
+    {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,27 +1,42 @@
-import { View, Image, FlatList } from 'react-native';
-
+import { useEffect, useState } from 'react';
+import { Image, FlatList } from 'react-native';
 import logoImg from '../../assets/logo-nlw-esports.png'
-
 import { Heading}  from '../../components/Heading';
-
 import { styles } from './styles';
-
-import { Gamecard } from '../../components/Gamecard';
-
-import { GAMES } from '../../utils/games';
+import { Gamecard, GameCardProps } from '../../components/Gamecard';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Background from '../../components/Background';
+import {useNavigation} from '@react-navigation/native';
 
 export function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps)  {
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
+  useEffect(() => {
+    fetch('http://192.168.0.8:3333/games')
+    .then((response) => response.json())
+    .then(data => setGames(data))
+  }, []);
+
   return (
-    <View style={styles.container}>
+
+    
+    <Background>
+    <SafeAreaView style={styles.container}>
 
       <Image source={logoImg} style={styles.logo}/>
 
       <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
 
-      <FlatList data={GAMES}
+      <FlatList data={games}
       keyExtractor={item => item.id}  
       renderItem={({ item }) => (
-        <Gamecard  data={item}/>
+        <Gamecard  data={item} onPress={() => handleOpenGame(item)}/>
         
       )}
       showsHorizontalScrollIndicator={false}
@@ -31,6 +46,7 @@ export function Home() {
      
 
       
-    </View>
+    </SafeAreaView>
+    </Background>
   );
 }
